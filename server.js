0e835439dfb746a1404aaa6da0f9c363fce89bce
Keith Underwood <keithunderwood510@gmail.com>
2017-11-15 16:02:38 -0800

    Add function to get all available extensions

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const bodyParser = require('body-parser') 
-const axios = require('axios')
+const request = require('request')
 const cronJob = require('cron').CronJob
 
 const app = express()
@@ -12,41 +12,47 @@ app.use(helmet())
 app.use(morgan('tiny'))
 app.use(bodyParser.json())
 
-async function getExtensions(offset) {
-  // 25 might be max limit
-  // need to post recursively while incrementing offset value by 25
-  // when POST results extensions array returns null, there is not more extensions
-  let postData = {
-    "limit": 25,
-    "offset": offset,
-    "sorts": [
-      {
-        "field":"popularity",
-        "direction":"desc"
-      }
-    ]
-  }
-  try {
-    const extensions = axios({
-      method: 'post',
-      url: 'https://api.twitch.tv/v5/extensions/search',
-      headers: {
-        "Accept": `application/vnd.twitchtv.v5+json`,
-        "Authorization": `OAuth ${process.env.OAUTH}`,
-      },
-      body: JSON.stringify(postData)
-    })
-
-    const results = await extensions
-    return results
-  } catch (error) {
-    console.log(error)
-  }
+// 25 might be max limit
+const EXTENSION_LIMIT = 25
+let allExt = []
+let initPostData = {
+  "limit": EXTENSION_LIMIT,
+  "offset": 0,
+  "sorts": [
+    {
+      "field":"popularity",
+      "direction":"desc"
+    }
+  ]
 }
 
-// TODO: need to build function to recursively call getExtensions
-// if the extensions array in the return data is null, there are no more extensions
-// need to then filter the extensions by ones that have the key/val {"state": "Released"}
+function getExtensions(postData) {
+  let options = {
+  method: 'POST',
+  url: 'https://api.twitch.tv/v5/extensions/search',
+  headers: {
+    'content-type': 'application/json',
+    authorization: `OAuth ${process.env.OAUTH}`,
+    accept: 'application/vnd.twitchtv.v5+json' },
+    body: postData,
+    json: true 
+  }
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    if (body.extensions) {
+      console.log('pushing', body.extensions.length)
+      allExt.push(...body.extensions)
+      postData.offset += EXTENSION_LIMIT
+      getExtensions(postData)
+    } else {
+      console.log(allExt.length)
+    }
+  })
+}
+
+// TODO: to then filter the extensions by ones that have the key/val {"state": "Released"}
 
 const port = process.env.PORT || 3000
 app.listen(port, console.log(`listening on port ${port}`))
